65bc9bc2f9441aabbf1fbde328089c9a70f7a4f6
EshietIy <eshietinyang@gmail.com>
2022-06-02 16:39:34 +0100

    solved

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,19 +7,17 @@ def makeChange(coins, total):
     """Determines the fewest number of coins needed to meet a given
     the given total
     """
+
     if total <= 0:
         return 0
-    tem = total
-    count = 0
-    i = 0
-    sortcoin = sorted(coins, reverse=True)
-    l = len(coins)
-    while tem > 0:
-        if i >= n:
-            return -1
-        if tem - sortcoin[i] >= 0:
-            rem -= sortcoin[i]
-            count += 1
-        else:
-            i += 1
-    return count
+
+    current_total = 0
+    coin_used = 0
+    coins = sorted(coins, reverse=True)
+    for coin in coins:
+        r = (total-current_total)//coin
+        current_total += r*coin
+        coin_used += r
+        if current_total == total:
+            return coin_used
+    return -1
