9268b9f919c9cfe5195baf1ed36d5e8a4af870af
EshietIy <eshietinyang@gmail.com>
2022-06-09 16:54:11 +0100

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,19 +9,17 @@ def island_perimeter(grid):
     perimeter = 0
     if type(grid) != list:
         return 0
-    
-    perimeter = 0
-    count = 0
-    for x, row in enumerate(grid):
-        for y, itm in enumerate(row):
-            if itm == 1 :
-                if grid[x - 1][y] == 0:
-                    perimeter = perimeter + 1
-                if grid[x + 1][y] == 0:
-                    perimeter = perimeter + 1
-                if grid[x][y - 1] == 0:
-                    perimeter = perimeter + 1
-                if grid[x][y - 1] == 0:
-                    perimeter = perimeter + 1
-                
+    n = len(grid)
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
+            perimeter += sum(edges)
     return perimeter
